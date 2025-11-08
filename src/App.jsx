d8362159e618d4d03190eed851c7355dd2750a04
MADBLOX252 <dg.aravindh@gmail.com>
2025-11-08 10:44:08 +0530

    Change Router to HashRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Tos from "./pages/Tos";
 
@@ -15,3 +15,4 @@ function App() {
 }
 
 export default App;
+
